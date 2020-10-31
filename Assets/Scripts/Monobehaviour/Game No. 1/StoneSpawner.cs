4aebe0fb0bd5e8cfcc6f277229eb14dc7bd2656e
Luxuride <luxuridegaming@gmail.com>
2020-11-01 00:08:35 +0100

    Project init

---
+++ Assets/Scripts/Monobehaviour/Game No. 1/StoneSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class StoneSpawner : MonoBehaviour
+{
+    public GameObject stonePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateStone();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateStone()
+    {
+        Instantiate(stonePrefab, new Vector3(-0.5f, 2f, -2.5f), Quaternion.identity);
+    }
+}
